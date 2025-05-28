5b0b2ac89c6d6f3b7524fafdfb0fa789a2e090df
Farhantigadi <farhantigadi123@gmail.com>
2025-05-28 09:23:12 +0530

    implemented put and delete mapping

--- src/main/java/com/EDigest/jounalAPP/Controller/JournalC.java
+++ src/main/java/com/EDigest/jounalAPP/Controller/JournalC.java
@@ -1,2 +1,47 @@
-package com.EDigest.jounalAPP.Controller;public class JournalC {
+package com.EDigest.jounalAPP.Controller;
+
+import com.EDigest.jounalAPP.Entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+@RestController
+@RequestMapping("journal")
+public class JournalC {
+
+    // In-memory database simulation
+    public Map<Integer, JournalEntity> journalEntry = new HashMap<>();
+
+    // Get all journals
+    @GetMapping
+    public List<JournalEntity> getAll() {
+        return new ArrayList<>(journalEntry.values());
+    }
+
+    // Create a journal
+    @PostMapping
+    public boolean post(@RequestBody JournalEntity journal) {
+        journalEntry.put(journal.getId(), journal);
+        return true;
+    }
+
+    // Get journal by ID
+    @GetMapping("id/{id}")
+    public JournalEntity getJournalById(@PathVariable int id) {
+        return journalEntry.get(id);
+    }
+
+    // Delete journal by ID
+    @DeleteMapping("id/{id}")
+    public JournalEntity deleteById(@PathVariable int id) {
+        return journalEntry.remove(id);
+    }
+
+    // Update journal by ID
+    @PutMapping("id/{id}")
+    public JournalEntity changeById(@PathVariable int id, @RequestBody JournalEntity updatedJournal) {
+        journalEntry.put(id, updatedJournal);
+        return updatedJournal;
+    }
 }
--- src/main/java/com/EDigest/jounalAPP/Entity/JournalEntity.java
+++ src/main/java/com/EDigest/jounalAPP/Entity/JournalEntity.java
@@ -1,2 +1,32 @@
-package com.EDigest.jounalAPP.Entity;public class JournalEntity {
+package com.EDigest.jounalAPP.Entity;
+
+public class JournalEntity {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJournalTitle() {
+        return journalTitle;
+    }
+
+    public void setJournalTitle(String journalTitle) {
+        this.journalTitle = journalTitle;
+    }
+
+    public String getJornalDescription() {
+        return jornalDescription;
+    }
+
+    public void setJornalDescription(String jornalDescription) {
+        this.jornalDescription = jornalDescription;
+    }
+
+    private String journalTitle;
+    private String jornalDescription;
 }
