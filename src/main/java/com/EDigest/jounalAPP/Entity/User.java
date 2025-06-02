32abe2c1aa7a8c81e0333459b44ee732d0f040ef
Farhantigadi <farhantigadi123@gmail.com>
2025-06-02 13:43:49 +0530

    added security dependency and also in appln prop added username and pass and verified through postman api

--- src/main/java/com/EDigest/jounalAPP/Entity/User.java
+++ src/main/java/com/EDigest/jounalAPP/Entity/User.java
@@ -22,5 +22,21 @@ public class User {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<JournalEntity> journals;
+
+    /*
+    @NonNull: Logbook's annotation (ensures these fields aren't null at runtime).
+
+   @Column(unique = true): Username must be unique.
+
+   @OneToMany(...): A user can have many journals.
+
+   cascade = CascadeType.ALL: If you save/delete a user, the journal entries are also saved/deleted.
+
+     orphanRemoval = true tells JPA:
+     If you remove a child object (like a journal) from the parent's list (like user's journal list), then delete it from the database too.
+
+
+     */
+
 }
 
